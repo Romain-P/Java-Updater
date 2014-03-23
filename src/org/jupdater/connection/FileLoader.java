aa6c170793c2634f0fc799da643cc4866137f5e3
John-r <skryn01@gmail.com>
2014-03-23 03:57:42 +0100

    Updater finished (console version)

--- src/org/jupdater/connection/FileLoader.java
+++ src/org/jupdater/connection/FileLoader.java
@@ -3,45 +3,70 @@ package org.jupdater.connection;
 import org.apache.commons.io.IOUtils;
 import org.jupdater.core.Config;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FileLoader {
     //path, file
     private Map<String, File> files = new HashMap<>();
+    private ExecutorService threadWorker = Executors.newSingleThreadExecutor();
 
     public void launchUpdate() {
-        loadDirectoryFiles(Config.getInstance().getVersionPath());
+        System.out.println("You will be updated to the version "+Config.getInstance().getVersionData());
 
-        for(Map.Entry<String, File> mFile: this.files.entrySet()) {
-            File file = new File(mFile.getKey());
-            try {
-                File folders = new File(file.getParent());
-                if(!folders.exists()) {
-                    folders.mkdirs();
-                }
-                if(!file.isFile() || !file.exists()) {
-                    file.createNewFile();
-                    file.setLastModified(mFile.getValue().lastModified());
-                }
+        threadWorker.execute(() -> {
+            loadDirectoryFiles("folder");
 
-                updateFile(file, mFile.getValue());
-            } catch(Exception e) {
-                System.out.println("An error was found.. error with the file ("+file.getName()+"): "+e.getMessage());
+            for(Map.Entry<String, File> mFile: files.entrySet()) {
+                File file = new File(mFile.getKey());
+                try {
+                    File folders = new File(file.getParent());
+                    if(!folders.exists()) {
+                        folders.mkdirs();
+                    }
+                    if(!file.isFile() || !file.exists()) {
+                        file.createNewFile();
+                        file.setLastModified(mFile.getValue().lastModified());
+                    }
+
+                    updateFile(file, mFile.getValue());
+                } catch(Exception e) {
+                    System.out.println("An error was found.. error with the file ("+file.getName()+"): "+e.getMessage());
+                }
             }
-        }
+
+            System.out.println("Folder updated to version "+Config.getInstance().getVersionData()+" with success !");
+            threadWorker.shutdown();
+            System.exit(0);
+        });
     }
 
     private void updateFile(File file, File futureFile) {
-        if(file.length() != 0)
-            file.delete();
-        else if (file.length() != futureFile.length()
+        if (file.length() != futureFile.length()
                 || file.lastModified() < futureFile.lastModified()) {
-            file.delete();
+
+            if(file.length() != 0) {
+                while(!file.delete()) {
+                    try {
+                        Thread.sleep(50);
+                    } catch(Exception e) {}
+                }
+            }
 
             try {
-                futureFile.createNewFile();
+                InputStream input = new FileInputStream(futureFile);
+                OutputStream output = new FileOutputStream(file);
+
+                if(!file.exists())
+                    file.createNewFile();
+
+                IOUtils.copy(input, output);
+
+                input.close();
+                output.close();
             } catch(Exception e) {
                 System.out.println("An error was found.. error with the file (" + file.getName() + "): " + e.getMessage());
             }
--- src/org/jupdater/core/Main.java
+++ src/org/jupdater/core/Main.java
@@ -2,13 +2,21 @@ package org.jupdater.core;
 
 import org.jupdater.connection.FileLoader;
 
+import java.io.File;
+
 public class Main {
     /**
      *
      * @author Romain-P
      */
     public static void main(String[] args) {
-        FileLoader fileLoader = new FileLoader();
-        fileLoader.launchUpdate();
+        File requiredFile = new File(Config.getInstance().getRequiredFile());
+        if(!requiredFile.exists()) {
+            System.out.println("You must put the updater into the good folder (which contains "+requiredFile.getPath()+")");
+            System.exit(1);
+        } else {
+            FileLoader fileLoader = new FileLoader();
+            fileLoader.launchUpdate();
+        }
     }
 }
