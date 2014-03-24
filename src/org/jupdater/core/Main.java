f83f59a153405d971d1da3d050e4ce1e89fcfe0f
John-r <skryn01@gmail.com>
2014-03-24 18:31:35 +0100

    Defining the default proxy if there is someone

--- src/org/jupdater/core/Main.java
+++ src/org/jupdater/core/Main.java
@@ -2,6 +2,9 @@ package org.jupdater.core;
 
 import java.io.File;
 
+import com.sun.deploy.net.proxy.DeployProxySelector;
+import com.sun.deploy.services.PlatformType;
+import com.sun.deploy.services.ServiceManager;
 import org.jupdater.connection.FileLoader;
 import org.jupdater.connection.VersionLoader;
 
@@ -11,6 +14,7 @@ public class Main {
      * @author Romain-P
      */
     public static void main(String[] args) {
+        initializeDefaultProxy();
     	VersionLoader.initializeVersion();
     	
         File requiredFile = new File(Config.getInstance().getRequiredFile());
@@ -20,5 +24,15 @@ public class Main {
             FileLoader fileLoader = new FileLoader();
             fileLoader.launchUpdate();
         }
+    }
+
+    private static void initializeDefaultProxy() {
+        ServiceManager.setService(System.getProperty("os.name").toLowerCase().indexOf("windows") != -1
+                        ? PlatformType.STANDALONE_TIGER_WIN32
+                        : PlatformType.STANDALONE_TIGER_MACOSX);
+        try {
+            // This will call ProxySelector.setDefault():
+            DeployProxySelector.reset();
+        } catch (Throwable throwable) {}
     }
 }
