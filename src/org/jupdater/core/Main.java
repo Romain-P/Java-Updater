22e5fcfc117715ee2cf19d916dfbf0d210738d8a
John-r <skryn01@gmail.com>
2014-03-24 21:58:17 +0100

    Disable proxy selector (some errors ><)

--- src/org/jupdater/core/Main.java
+++ src/org/jupdater/core/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         //defining default proxy
-        initializeDefaultProxy();
+        //initializeDefaultProxy();
 
         //initialize host releases path
     	VersionLoader.initializeVersion();
